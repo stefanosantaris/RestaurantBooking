750dbd20af9e5cc9c6661d0689f7e730eadb0aa7
Stefanos Antaris <stefanos.antaris@hivestreaming.com>
2022-01-23 22:47:12 +0100

    Added custom action

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,20 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.knowledge_base.actions import ActionOpenMap
+
+import os
+
+
+class OpenMap(ActionOpenMap):
+
+    def name(self) -> Text:
+        return "action_open_map"
+
+    def run(self, dispatcher: CollectingDispatcher) :
+        os.system("open https://g.page/menomale-kungsholmen?share")
+        
